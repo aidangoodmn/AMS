8959392ca03dbf1d62e6f051f4728ce23fe4e86f
Aidan Goodman <aidangoodmn@gmail.com>
2019-01-19 11:36:59 -0800

    1.0.4

--- web_dev/NODEJS.js
+++ web_dev/NODEJS.js
@@ -6,11 +6,24 @@ const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+app.get('/', function(req, res){
+    res.render('index');
+})
+
+let toggleMENUstatus = false;
+
 app.listen(3000, function(){
     console.log('Server started on port 3000');
 })
+
+
+
+
